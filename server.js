56292c23b164191c4373a125f6771a2121222a0e
RABIEsyr <rabie.alkholi@gmail.com>
2019-04-07 00:11:22 +0300

    first commit

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 const file1 = require('./socket/socket')(io);
+const path = require('path');
+// const moment = require('moment');
+ const expressSS = require('express');
 
-const moment = require('moment');
-// const express = require('express');
 const bodyparser = require('body-parser')
 const morgan = require('morgan');
 const mongoose = require('mongoose');
@@ -30,24 +31,34 @@ mongoose.connect(config.db, err => {
 
 // let app = express();
 
+  app.use(expressSS.static(path.join(__dirname, 'deploy'))); 
 
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(cors());
 
-// use routes
-app.use('/api', apiRoute);
-app.use('/shopping', shoppingRoute);
-app.use('/', indexRoute);
-app.use('/charg-card', chargeCardRoute)
 
+
+
+// use routes
 setInterval( ()=> file1 ,1000);
 
-const port = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/deploy/index.html'));
+});
+
+app.use('/api', apiRoute);
+app.use('/shopping', shoppingRoute);
+app.use('/charg-card', chargeCardRoute)
+
+app.use('/', indexRoute);
+
+
+const port =  process.env.PORT || 8000;
 http.listen(port, err => {
     if (err) throw err;
-    console.log(`serer running on port:  ${config.port}`)
+    console.log(`serer running on port:  ${port}`)
 })
 
