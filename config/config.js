0620ab410bb8aa8ee4f09b01f11d6369fc61d711
RABIEsyr <rabie.alkholi@gmail.com>
2019-04-07 09:18:00 +0300

    second commit

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 
 module.exports = {
-    db: 'mongodb://localhost/RezekNewMarket',
+   // db: 'mongodb://localhost/RezekNewMarket',
     port: 3000,
     secret: '333',
-    PRIVATE_KEY: 'sk_test_vllYrL6F5rd32Vp9V1Pn1UqA00zY8JrlNs'
+    PRIVATE_KEY: 'sk_test_vllYrL6F5rd32Vp9V1Pn1UqA00zY8JrlNs',
+    db: "mongodb+srv://rabie:a1b2c3d4e5@cluster0-qjvqq.mongodb.net/test?retryWrites=true"
 };
