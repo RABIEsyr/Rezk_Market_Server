934b2e356d948f8c3ee68c308931758d3ec7f4db
RABIEsyr <rabie.alkholi@gmail.com>
2019-04-07 09:50:19 +0300

    second commit

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     port: 3000,
     secret: '333',
     PRIVATE_KEY: 'sk_test_vllYrL6F5rd32Vp9V1Pn1UqA00zY8JrlNs',
-    db: "atlas_db"
+    db: "mongodb+srv://rabie:1111@rezkmarket-qbue6.mongodb.net/test?retryWrites=true"
 }
