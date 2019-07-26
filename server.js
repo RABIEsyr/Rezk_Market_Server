858690cd1f18dae88bba0e856dc94fb91a2d9b9a
RABIEsyr <rabie.alkholi@gmail.com>
2019-07-26 12:40:17 +0300

    new commit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connect(config.db, err => {
 // let app = express();
 
 
-  app.use(expressSS.static(path.join(__dirname, 'news-app'))); 
+  app.use(expressSS.static(path.join(__dirname, 'deploy'))); 
 
 
 app.use(bodyparser.json());
@@ -42,22 +42,22 @@ app.use(cors());
 
 
 
+
+
+// use routes
+// setInterval( ()=> file1 ,1000);
+
+ app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/deploy/index.html'));
+}); 
+
+
+
 app.use('/api', apiRoute);
 app.use('/shopping', shoppingRoute);
 app.use('/charg-card', chargeCardRoute)
 
 app.use('/', indexRoute);
-
-// use routes
-//setInterval( ()=> file1 ,1000);
-
-//  app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/news-app/index.html'));
-// }); 
-
-
-
-
 
 const port = 3000 // process.env.PORT || 8000;
 http.listen(port, err => {
